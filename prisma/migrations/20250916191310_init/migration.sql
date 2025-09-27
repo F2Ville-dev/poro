6e5767c3fc7ba6d7af125737c919831ae92eaec3
F2Ville <contact@f2ville.dev>
2025-09-27 02:04:35 +0200

    edit .env.example and remove migrations

--- prisma/migrations/20250916191310_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Notification" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "type" TEXT NOT NULL,
-    "channelId" TEXT NOT NULL,
-    "creator" TEXT NOT NULL,
-    "lastSent" DATETIME
-);
